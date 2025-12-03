db3fc3dc15235e63ad73f71899142d7cf27f2d3e
Felix <23635466+its-felix@users.noreply.github.com>
2025-12-03 13:46:32 +0100

    Day 3 Part 2

--- day03/day03.go
+++ day03/day03.go
@@ -4,31 +4,48 @@ import (
 	"iter"
 )
 
-func SolvePart1(input <-chan string) int {
-	sum := 0
+func SolvePart1(input <-chan string) uint64 {
+	return solve(input, 2)
+}
+
+func SolvePart2(input <-chan string) uint64 {
+	return solve(input, 12)
+}
+
+func solve(input <-chan string, batteries int) uint64 {
+	sum := uint64(0)
 
 	for bank := range parse(input) {
-		high := [2]uint8{0, 0}
+		high := make([]uint8, batteries)
 
-		for i := 0; i < len(bank); i++ {
-			if bank[i] > high[0] && (i+1) < len(bank) {
-				high[0] = bank[i]
-				high[1] = bank[i+1]
-			} else if bank[i] > high[1] {
-				high[1] = bank[i]
+		for i := 0; i < batteries; i++ {
+			remainingPosToFill := batteries - i
+			bestIdxForPos := 0
+
+			for j, v := range bank {
+				if (len(bank) - j) < remainingPosToFill {
+					break
+				}
+
+				if v > bank[bestIdxForPos] {
+					bestIdxForPos = j
+				}
 			}
+
+			high[i] = bank[bestIdxForPos]
+			bank = bank[bestIdxForPos+1:]
 		}
 
-		num := (high[0] * 10) + high[1]
-		sum += int(num)
+		num := uint64(0)
+		for _, v := range high {
+			num *= 10
+			num += uint64(v)
+		}
+
+		sum += num
 	}
 
 	return sum
-}
-
-func SolvePart2(input <-chan string) int {
-	parse(input)
-	return 0
 }
 
 func parse(input <-chan string) iter.Seq[[]uint8] {
