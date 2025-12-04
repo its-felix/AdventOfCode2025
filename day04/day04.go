95003b6f9456b087dce81313028738f3b8e26f35
Felix <23635466+its-felix@users.noreply.github.com>
2025-12-04 10:13:27 +0100

    Day 4 Part 2

--- day04/day04.go
+++ day04/day04.go
@@ -23,22 +23,52 @@ func SolvePart1(input <-chan string) int {
 }
 
 func SolvePart2(input <-chan string) int {
-	parse(input)
-	return 0
-}
-
-func countAdjacent(grid [][]rune, currRow, currCol int, match rune) int {
+	grid := parse(input)
 	count := 0
 
-	for row := max(currRow-1, 0); row <= min(currRow+1, len(grid)-1); row++ {
-		for col := max(currCol-1, 0); col <= min(currCol+1, len(grid[row])-1); col++ {
-			if (row != currRow || col != currCol) && grid[row][col] == match {
-				count++
-			}
+	for row := 0; row < len(grid); row++ {
+		for col := 0; col < len(grid[row]); col++ {
+			count += processPosition(grid, row, col)
 		}
 	}
 
 	return count
+}
+
+func processPosition(grid [][]rune, row, col int) int {
+	removed := 0
+
+	if grid[row][col] == roll {
+		adjacentRolls := findAdjacent(grid, row, col, roll)
+		if len(adjacentRolls) < 4 {
+			grid[row][col] = space
+			removed++
+
+			for _, pos := range adjacentRolls {
+				removed += processPosition(grid, pos[0], pos[1])
+			}
+		}
+	}
+
+	return removed
+}
+
+func countAdjacent(grid [][]rune, currRow, currCol int, match rune) int {
+	return len(findAdjacent(grid, currRow, currCol, match))
+}
+
+func findAdjacent(grid [][]rune, currRow, currCol int, match rune) [][2]int {
+	positions := make([][2]int, 0)
+
+	for row := max(currRow-1, 0); row <= min(currRow+1, len(grid)-1); row++ {
+		for col := max(currCol-1, 0); col <= min(currCol+1, len(grid[row])-1); col++ {
+			if (row != currRow || col != currCol) && grid[row][col] == match {
+				positions = append(positions, [2]int{row, col})
+			}
+		}
+	}
+
+	return positions
 }
 
 func parse(input <-chan string) [][]rune {
