4fb5ec0eb26f5efffee1fc7b35670b41a9b96f67
Felix <23635466+its-felix@users.noreply.github.com>
2025-12-06 08:01:42 +0100

    Day 6 Part 1

---
+++ day06/day06.go
@@ -0,0 +1,90 @@
+package day06
+
+import (
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+const (
+	operationMul = rune('*')
+	operationAdd = rune('+')
+)
+
+type problem struct {
+	numbers   []int
+	operation rune
+}
+
+func SolvePart1(input <-chan string) int {
+	sum := 0
+	for _, p := range parse(input) {
+		opResult := p.numbers[0]
+
+		switch p.operation {
+		case operationMul:
+			for _, n := range p.numbers[1:] {
+				opResult *= n
+			}
+
+		case operationAdd:
+			for _, n := range p.numbers[1:] {
+				opResult += n
+			}
+		}
+
+		sum += opResult
+	}
+	return sum
+}
+
+func SolvePart2(input <-chan string) int {
+	parse(input)
+	return 0
+}
+
+func parse(input <-chan string) []problem {
+	minLength, maxLength := math.MaxInt, 0
+	numbers := make([][]int, 0)
+	problems := make([]problem, 0)
+
+	for line := range input {
+		if line == "" {
+			continue
+		}
+
+		numberOrSymbols := strings.FieldsFunc(line, unicode.IsSpace)
+		minLength, maxLength = min(minLength, len(numberOrSymbols)), max(maxLength, len(numberOrSymbols))
+
+		if minLength != maxLength {
+			panic("invalid input")
+		}
+
+		switch rune(numberOrSymbols[0][0]) {
+		case operationMul, operationAdd:
+			for i, symbol := range numberOrSymbols {
+				problems = append(problems, problem{
+					numbers:   numbers[i],
+					operation: rune(symbol[0]),
+				})
+			}
+
+		default:
+			for i, number := range numberOrSymbols {
+				for i >= len(numbers) {
+					numbers = append(numbers, make([]int, 0, len(numberOrSymbols)))
+				}
+
+				num, err := strconv.Atoi(number)
+				if err != nil {
+					panic(err)
+				}
+
+				numbers[i] = append(numbers[i], num)
+			}
+		}
+	}
+
+	return problems
+}
