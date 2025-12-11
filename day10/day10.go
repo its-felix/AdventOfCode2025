dea1d1ad2807339276b5cebb18bda4a9ec80cfdc
Felix <23635466+its-felix@users.noreply.github.com>
2025-12-11 09:38:06 +0100

    Day 10 Part 2 attempt

--- day10/day10.go
+++ day10/day10.go
@@ -1,20 +1,26 @@
 package day10
 
 import (
+	"context"
 	"fmt"
+	"iter"
 	"math"
+	"slices"
 	"strconv"
 	"strings"
+	"sync/atomic"
+
+	"golang.org/x/sync/errgroup"
 )
 
-type Machine struct {
+type Machine[B any] struct {
 	TargetState uint64
-	Buttons     []uint64
+	Buttons     []B
 	Joltages    []int
 }
 
 func SolvePart1(input <-chan string) int {
-	machines := parse(input)
+	machines := parse(input, parseButtonPart1)
 	sum := 0
 
 	for _, m := range machines {
@@ -53,19 +59,128 @@ func solvePart1(state uint64, buttons []uint64, targetState uint64, maxPresses i
 	return maxPresses, success
 }
 
-func SolvePart2(input <-chan string) int {
-	parse(input)
-	return 0
+func SolvePart2(input <-chan string) uint64 {
+	machines := parse(input, parseButtonPart2)
+
+	var sum atomic.Uint64
+	g, ctx := errgroup.WithContext(context.Background())
+
+	for i, m := range machines {
+		g.Go(func() error {
+			presses, solvable, err := solvePart2(ctx, make([]int, len(m.Joltages)), m.Buttons, m.Joltages, math.MaxInt)
+			if err != nil {
+				return err
+			}
+
+			if !solvable {
+				return fmt.Errorf("machine %d: not solvable", i+1)
+			}
+
+			fmt.Printf("machine %d: %d presses\n", i+1, presses)
+
+			sum.Add(uint64(presses))
+
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
+
+	return sum.Load()
 }
 
-func parse(input <-chan string) []Machine {
-	machines := make([]Machine, 0)
+func solvePart2(ctx context.Context, state []int, buttons [][]int, targetState []int, maxPresses int) (int, bool, error) {
+	select {
+	case <-ctx.Done():
+		return 0, false, ctx.Err()
+
+	default:
+	}
+
+	solved, possible := isSolvedPart2(state, targetState)
+	if solved {
+		return 0, true, nil
+	} else if !possible {
+		return 0, false, nil
+	} else if maxPresses < 1 {
+		return 0, false, nil
+	}
+
+	for i := 0; i < len(buttons); i++ {
+		for presses, state := range applyButtonManyTimes(state, buttons[i], targetState) {
+			if presses > maxPresses {
+				break
+			}
+
+			additionalPresses, wasSolved, err := solvePart2(ctx, state, buttons[i+1:], targetState, maxPresses-presses)
+			if err != nil {
+				return 0, false, err
+			}
+
+			if wasSolved {
+				solved = true
+				presses += additionalPresses
+
+				if presses < maxPresses {
+					maxPresses = presses
+				}
+			}
+		}
+	}
+
+	return maxPresses, solved, nil
+}
+
+func applyButtonManyTimes(state, button, targetState []int) iter.Seq2[int, []int] {
+	return func(yield func(int, []int) bool) {
+		presses := 0
+		for {
+			state = applyButtonPart2(state, button)
+			presses++
+			if _, possible := isSolvedPart2(state, targetState); !possible {
+				break
+			}
+
+			if !yield(presses, state) {
+				break
+			}
+		}
+	}
+}
+
+func applyButtonPart2(state, button []int) []int {
+	result := slices.Clone(state)
+	for _, idx := range button {
+		result[idx]++
+	}
+
+	return result
+}
+
+func isSolvedPart2(state, targetState []int) (bool, bool) {
+	solved := true
+
+	for i := 0; i < len(state); i++ {
+		if state[i] > targetState[i] {
+			return false, false
+		} else if state[i] < targetState[i] {
+			solved = false
+		}
+	}
+
+	return solved, true
+}
+
+func parse[B any](input <-chan string, parseButton func(string) (B, error)) []Machine[B] {
+	machines := make([]Machine[B], 0)
 	for line := range input {
 		if line == "" {
 			continue
 		}
 
-		m, err := parseMachine(line)
+		m, err := parseMachine(line, parseButton)
 		if err != nil {
 			panic(err)
 		}
@@ -76,8 +191,8 @@ func parse(input <-chan string) []Machine {
 	return machines
 }
 
-func parseMachine(line string) (Machine, error) {
-	var m Machine
+func parseMachine[B any](line string, parseButton func(string) (B, error)) (Machine[B], error) {
+	var m Machine[B]
 
 	if line[0] != '[' {
 		return m, fmt.Errorf("invalid line: %q", line)
@@ -95,7 +210,12 @@ func parseMachine(line string) (Machine, error) {
 		return m, err
 	}
 
-	line, m.Buttons, err = parseButtons(line[end+1:])
+	line, m.Buttons, err = parseButtons(line[end+1:], parseButton)
+	if err != nil {
+		return m, err
+	}
+
+	m.Joltages, err = parseJoltages(line)
 	if err != nil {
 		return m, err
 	}
@@ -125,8 +245,8 @@ func parseIndicators(indicators string) (uint64, error) {
 	return result, nil
 }
 
-func parseButtons(line string) (string, []uint64, error) {
-	buttons := make([]uint64, 0)
+func parseButtons[B any](line string, parseButton func(string) (B, error)) (string, []B, error) {
+	buttons := make([]B, 0)
 
 	start := strings.IndexRune(line, '(')
 	for start != -1 {
@@ -150,7 +270,28 @@ func parseButtons(line string) (string, []uint64, error) {
 	return line, buttons, nil
 }
 
-func parseButton(button string) (uint64, error) {
+func parseJoltages(joltages string) ([]int, error) {
+	start := strings.IndexRune(joltages, '{')
+	end := strings.IndexRune(joltages, '}')
+	if start == -1 || end == -1 {
+		return nil, fmt.Errorf("invalid line, missing joltages: %q", joltages)
+	}
+
+	result := make([]int, 0)
+
+	for _, v := range strings.Split(joltages[start+1:end], ",") {
+		idx, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, idx)
+	}
+
+	return result, nil
+}
+
+func parseButtonPart1(button string) (uint64, error) {
 	result := uint64(0)
 
 	for _, v := range strings.Split(button, ",") {
@@ -164,6 +305,21 @@ func parseButton(button string) (uint64, error) {
 		}
 
 		result |= 1 << uint64(idx)
+	}
+
+	return result, nil
+}
+
+func parseButtonPart2(button string) ([]int, error) {
+	result := make([]int, 0)
+
+	for _, v := range strings.Split(button, ",") {
+		idx, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, idx)
 	}
 
 	return result, nil
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/its-felix/AdventOfCode2025
 
 go 1.25
+
+require golang.org/x/sync v0.19.0
